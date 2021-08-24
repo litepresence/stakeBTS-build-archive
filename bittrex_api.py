d395c11258d36cbbb82bc7ec8f509c7327fb7e1c
litepresence <finitestate@tutamail.com>
2021-08-23 20:15:54 -0400

    with statements for proper closing of TCP

--- bittrex_api.py
+++ bittrex_api.py
@@ -1,35 +1,27 @@
 """
 Bittrex Authenticated Ops
-
 Forked:
     https://github.com/jchysk/python-bittrex-v3-sdk/blob/master/client.py
-
 Consulted:
     https://github.com/mkuenzie/bittrex/blob/master/bittrex/bittrex.py
     https://github.com/DevSecNinja/aiobittrexapi/blob/main/aiobittrexapi/utils.py
-
 Added:
     post_withdrawal()
     unit_test()
-
 Passes:
     pylint/black/isort/sourcery
-
 wtfpl litepresence2021
 """
-
 import hashlib
 import hmac
 import json
 import time
 from urllib.parse import urlencode
-
 import requests
 
 API_URL = "https://api.bittrex.com/v3/"
 API_KEY = ""  # only for unit testing
 API_SECRET = ""  # only for unit testing
-
 
 
 class Bittrex:
@@ -41,24 +33,24 @@ class Bittrex:
         self.response = ""
         self.api_key = api_key
         self.api_secret = api_secret
-        self.session = self._init_session()
 
-
-    def _init_session(self):
+    def _session(self):
         header = {
             "Content-Type": "application/json",
             "Accept": "application/json",
         }
-        session = requests.session()
+        session = requests.sessions.Session()
         session.headers.update(header)
         return session
 
     def _request(self, method, endpoint, **kwargs):
         uri = API_URL + endpoint
-        self.response = getattr(self.session, method)(uri, **kwargs)
+        with self._session() as session:
+            self.response = getattr(session, method)(uri, **kwargs)
         return self.response.json()
 
     def _authenticated_request(self, method, endpoint, **kwargs):
+
         request_data = {}
         payload = ""
         if len(kwargs) > 0:
@@ -66,28 +58,24 @@ class Bittrex:
                 endpoint = endpoint + "?" + urlencode(kwargs)
             else:
                 request_data["data"] = payload = json.dumps(kwargs)
-
         mill_timestamp = str(int(time.time() * 1000))
         content_hash = hashlib.sha512(payload.encode()).hexdigest()
         uri = API_URL + endpoint
         _pre_sign = mill_timestamp + uri + method.upper() + content_hash
-
         signature = hmac.new(
             self.api_secret.encode(), _pre_sign.encode(), hashlib.sha512
         ).hexdigest()
-        self.session.headers.update(
-            {
-                "Api-Key": self.api_key,
-                "Api-Timestamp": mill_timestamp,
-                "Api-Content-Hash": content_hash,
-                "Api-Signature": signature,
-            }
-        )
-
-        self.response = getattr(self.session, method)(uri, **request_data)
+        with self._session() as session:
+            session.headers.update(
+                {
+                    "Api-Key": self.api_key,
+                    "Api-Timestamp": mill_timestamp,
+                    "Api-Content-Hash": content_hash,
+                    "Api-Signature": signature,
+                }
+            )
+            self.response = getattr(session, method)(uri, **request_data)
         return self.response.json()
-
-
 
     def get_balances(self):
         """
@@ -105,18 +93,15 @@ class Bittrex:
         return self._authenticated_request("get", "balances")
 
     def post_withdrawal(self, **params):
-
         """
         https://api.bittrex.com/v3/withdrawals
         https://bittrex.github.io/api/v3#/definitions/NewWithdrawal
-
         @param currencySymbol str(upper) unique symbol of the currency to withdraw from
         @param quantity str(float()) quantity to withdraw
         @param cryptoAddress str() crypto address to withdraw funds to
         @param cryptoAddressTag str() (optional) custom message further specifying how
             to complete the withdrawal (may not be supported for this currency)
         @param clientWithdrawalId: str(uuid) (optional) idempotency client identifier
-
         :return:
         {
           "id": "string (uuid)",
@@ -136,7 +121,7 @@ class Bittrex:
         return self._authenticated_request("post", "withdrawals", **params)
 
     def get_addresses(self):
-        '''
+        """
         List deposit addresses that have been requested or provisioned
         :return:
         [
@@ -147,10 +132,8 @@ class Bittrex:
             "cryptoAddressTag": "string"
           }
         ]
-        '''
+        """
         return self._authenticated_request("get", "addresses")
-
-
 
 
 def unit_test():
@@ -158,7 +141,6 @@ def unit_test():
     sample withdrawal operation
     """
     bittrex_api = Bittrex(api_key=API_KEY, api_secret=API_SECRET)
-
     # POST WITHDRAWAL
     params = {
         "currencySymbol": "BTS",
@@ -167,11 +149,9 @@ def unit_test():
         "cryptoAddressTag": "hello world",
     }
     ret = bittrex_api.post_withdrawal(**params)
-
     print(ret)
-
     # need to get thailand VPN
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     unit_test()
