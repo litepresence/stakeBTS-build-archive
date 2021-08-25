5ecf0e996b78defb3236938fb7a07f08de9c549c
litepresence <finitestate@tutamail.com>
2021-08-24 23:34:50 -0400

    Add files via upload

---
+++ manual_payouts.py
@@ -0,0 +1,42 @@
+"""
+BitShares.org StakeMachine
+Hung Payment Handler
+BitShares Management Group Co. Ltd.
+"""
+# STANDARD PYTHON IMPORTS
+from getpass import getpass
+
+# STAKE BTS IMPORTS
+from stake_bitshares import sql_db, munix, payment_parent
+from config import BROKER
+
+
+def main():
+    """
+    Handle hung payments with status "processing" due to insufficient funds
+    """
+    print("\033c")
+    # read from database gather list of payments due
+    query = (
+        "SELECT amount, client, start, number, type FROM stakes "
+        + "WHERE (type='principal' OR type='interest') AND due<? "
+        + "AND status='processing'"
+    )
+    values = (munix(),)
+    payments_due = sql_db(query, values)
+    print(payments_due)
+    choice = input("\ny + Enter to make these payments, or just Enter to abort\n")
+
+    if choice == "y":
+        keys = {
+            "broker": BROKER,
+            "password": getpass(
+                f"\nInput Pybitshares Password for {BROKER} and press ENTER:\n"
+            ),
+        }
+        payment_parent(payments_due, keys)
+
+
+if __name__ == "__main__":
+
+    main()
